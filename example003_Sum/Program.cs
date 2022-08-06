9d1dd88f3229610bb1091e26e45976ff4b8dcce2
Yulia <rosiely@mail.ru>
2022-08-06 11:01:40 +0500

    example003 Sum Random

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumderA = 31;
-int NumderB = 5;
+﻿int NumderA = new Random().Next(1,10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(NumderA);
+int NumderB = new Random().Next(1,10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(NumderB);
 int result = NumderA + NumderB;
 Console.WriteLine(result);
--- example004_Div/Program.cs
+++ example004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double NumderA = 12;
+double NumderB = 5;
+double result = NumderA / NumderB;
+Console.WriteLine(result);
